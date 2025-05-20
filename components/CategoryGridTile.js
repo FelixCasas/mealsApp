aca8eae44dca316d6676656b579f6e780129fe26
Felix Casas <casasfelix30@gmail.com>
2025-05-19 19:23:50 -0700

    codigo en CategoryGridTile.js

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -24,6 +24,31 @@ const styles = StyleSheet.create({
         borderRadius: 8,
         elevation: 4,
         backgroundColor: 'White',
-        
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
+    },
+
+    button: {
+        flex: 1
+    },
+
+    buttonPressed: {
+        opacity: 0.5
+    },
+
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        borderRadius: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
     }
 })
