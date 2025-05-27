0a830c55802ed8b494556ccbd683ed5aea2c9f0d
Felix Casas <casasfelix30@gmail.com>
2025-05-26 22:41:01 -0700

    cambios

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -18,9 +18,9 @@ function MealDetailScree({route, navigation}) {
                 affordability={selectedMeal.affordability}
             />
             <View>
-                <Text style={styles.subtitle}>Ingredients</Text>
+                <Text style={styles.subtitle}>Notas</Text>
                 {selectedMeal.ingredients.map(ingredients => <Text style={styles.textContent} key={ingredients}>{ingredients}</Text>)}
-                <Text style={styles.subtitle}>Steps</Text>
+                <Text style={styles.subtitle}>Descripción</Text>
                 {selectedMeal.steps.map(step => <Text style={styles.textContent} key={step}>{step}</Text>)}
             </View>
         </ScrollView>
